c51c225168849719cf7df38300d6f17c83a9dceb
Darko <d.gjorgjievski96@gmail.com>
2024-09-10 00:52:56 +0200

    New class added MVP

---
+++ CodeChemSelenium/Tests/ButtonContact.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeChemSelenium.Tests
+{
+    public class ButtonContact
+    {
+        [Test]
+        public void FunctionalityofButtonContact()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://codechem.com/";
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("Contact", driver.FindElement(By.XPath("//div[@id='__next']/div/div/div/div/div/a[8]/button")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//div[@id='__next']/div/div/div/div/div/a[8]/button")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("Name")).Click();
+            driver.FindElement(By.Name("Name")).Clear();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("Name")).SendKeys("Darko Gjorgjievski");
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("Darko Gjorgjievski", driver.FindElement(By.Name("Name")).GetAttribute("value"));
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("Email")).Clear();
+            driver.FindElement(By.Name("Email")).SendKeys("d.gjorgjievski96@gmail.com");
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("d.gjorgjievski96@gmail.com", driver.FindElement(By.Name("Email")).GetAttribute("value"));
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("PhoneNumber")).Clear();
+            driver.FindElement(By.Name("PhoneNumber")).SendKeys("078847199");
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("078847199", driver.FindElement(By.Name("PhoneNumber")).GetAttribute("value"));
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("CompanyName")).Clear();
+            driver.FindElement(By.Name("CompanyName")).SendKeys("CodeChem");
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("CodeChem", driver.FindElement(By.Name("CompanyName")).GetAttribute("value"));
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//img[@alt='Checkbox unchecked']")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("Message")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("Message")).SendKeys("Imagine an application designed specifically for software testers. This app would include a bug tracker to log and prioritize issues, a test case manager to organize and execute test cases, and real-time collaboration tools to streamline communication with developers. It’s a one-stop solution to make testing more efficient and ensure software quality");
+            Thread.Sleep(2000);
+            driver.Close();
+        }
+    }
+}
---
+++ CodeChemSelenium/Tests/NavBar.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Runtime.InteropServices;
+
+namespace CodeChemSelenium.Tests
+{
+    public class NavBar
+    {
+        [Test]
+        public void FunctionalityofNavBar()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://codechem.com/";
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("MVP")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("MVP", driver.FindElement(By.LinkText("MVP")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Cases")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("Cases", driver.FindElement(By.LinkText("Cases")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Services")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("Services", driver.FindElement(By.LinkText("Services")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("How we work")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("How we work", driver.FindElement(By.LinkText("How we work")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("About")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("About", driver.FindElement(By.LinkText("About")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Careers")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("Careers", driver.FindElement(By.LinkText("Careers")).Text);
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Blog")).Click();
+            Thread.Sleep(2000);
+            ClassicAssert.AreEqual("Blog", driver.FindElement(By.LinkText("Blog")).Text);
+            Thread.Sleep(2000);
+            var tabs = driver.WindowHandles;
+            if (tabs.Count > 1)
+            {
+                driver.SwitchTo().Window(tabs[1]);
+                Thread.Sleep(2000);
+                driver.Close();
+                driver.SwitchTo().Window(tabs[0]);
+            }
+            Thread.Sleep(2000);
+            driver.Close();
+            
+        } 
+    }
+}
