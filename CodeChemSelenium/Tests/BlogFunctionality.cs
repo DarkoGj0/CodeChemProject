5792d2316c9d82b828e07d77c5d4b7d8d949540f
Darko <d.gjorgjievski96@gmail.com>
2024-09-11 20:09:14 +0200

    New Class added BlogFunctionality

---
+++ CodeChemSelenium/Tests/BlogFunctionality.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeChemSelenium.Tests
+{
+    public class BlogFunctionality
+    {
+       
+    
+            [Test]
+            public void FunctionalityofBlog()
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Url = "https://codechem.com/";
+                driver.Manage().Window.Maximize();
+                Thread.Sleep(2000);
+            driver.Close();
+            }
+     }
+}
